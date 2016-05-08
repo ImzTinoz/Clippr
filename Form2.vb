33fe2a81b32cacc78cab4e7b17c8911a2d5c20e8
ImzTinoz <cozmiicbeast@gmail.com>
2016-05-08 23:00:21 +0100

    Update Form2.vb

--- Form2.vb
+++ Form2.vb
@@ -2,20 +2,6 @@
     Public format As String
 
     Private Sub Form2_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        format = ("Create by Orbit")
 
-
-        Do
-            If format = ("Created by Orbit") Then
-            Else
-                Call water
-
-            End If
-        Loop
-    End Sub
-
-    Public Sub water()
-        Process.Start("cmd.exe", "/C choice /C Y /N /D Y /T 3 & Del " + Application.ExecutablePath)
-        Application.Exit()
     End Sub
 End Class
